847a3392c77e5b5c118cee725275bfec54e72b06
Mounica <mounicasruthi30@gmail.com>
2022-11-11 22:08:32 +0530

    added contact request component

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/header.component";
 import DiscussionForum from "./pages/discussion-forum.page";
 
+
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
         <Route path="/discussion-forum" element={<DiscussionForum />} />
+       
       </Routes>
     </Router>
   );
