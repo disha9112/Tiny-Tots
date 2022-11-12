4ee38ac465cf768c4655f51f124e3b70e175787a
Ishika <ishika4462@gmailcom>
2022-11-12 11:40:43 +0530

    add landing page

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/header.component";
 import DiscussionForum from "./pages/discussion-forum.page";
+import LandingPage from "./pages/landing.page";
 
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
+        <Route path="/" element={<LandingPage />} />
         <Route path="/discussion-forum" element={<DiscussionForum />} />
       </Routes>
     </Router>
